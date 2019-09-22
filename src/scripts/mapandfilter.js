2673e410ad589a3097dc60424d01b148f1cecb21
Aniyogakiza <aimable.niyogakiza@drpgroup.com>
2019-09-22 21:59:07 +0100

    fix eslint

--- src/scripts/mapandfilter.js
+++ src/scripts/mapandfilter.js
@@ -2,9 +2,9 @@
 const gt10 = n => n > 10;
 const addTen = n => n + 10;
 
-const items = [1, 2, 14, 12, 6, 29]
+const items = [1, 2, 14, 12, 6, 29];
 const itemOverTen = items.filter(gt10);
-const mapAddTen = map(addTen)
+const mapAddTen = map(addTen);
 const itemsAddTen = mapAddTen(items);
 
 const mapThenFilter = filter(gt10, mapAddTen([1, 2, -4, 3, 4, -5]));
@@ -57,7 +57,7 @@ const obj = {
     c: 34,
     d: 3,
     e:-3
-}
-const objMapAddTen = mapAddTen(obj)
+};
+const objMapAddTen = mapAddTen(obj);
 
 debugger;
